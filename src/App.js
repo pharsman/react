8daebdf0aedd24d340cef0723afa3094e6062eb8
Pharsman <tengo6531@gmail.com>
2023-03-06 19:22:38 +0400

    react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import Employees from "./Employees";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Employees />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
